94f4a0eda0370eff60efca22ea4611e158bf4f43
George Taray <gtaray@hotmail.com>
2026-02-04 14:53:03 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "gtaray.github.io/magitech-revolution",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
